dcbb5b478b1ffc8f0caf2ced314e73d0a5ebfd74
zohaib089 <zabi.jarral08@gmail.com>
2020-02-08 19:36:33 +0100

    User model of Csv file has been updated

--- django_csv_uploader/csvuploader/models.py
+++ django_csv_uploader/csvuploader/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    codice_cliente = models.IntegerField()
+    codice_fiscale = models.CharField(max_length=50)
+    ragione_sociale = models.TextField()
+    nome = models.CharField(max_length=50)
+    indirizzo = models.CharField(max_length=50)
+    cap = models.IntegerField()
+    comune = models.TextField()
+    provincia = models.TextField()
+    codice_stato = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.nome}'
+    
